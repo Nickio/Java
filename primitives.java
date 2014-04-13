8d2afd80e75cb050d708c176a84ad3ae4a426017
Nickio <nick_the_greek10@yahoo.com>
2014-04-13 18:13:56 +0300

    Primitives in Java
    Primitives in Java which I got of a tutorial on the web

---
+++ primitives.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // we intend to print this out ---> H3110 w0r1d 2.0 true
+		//using primitives
+		
+		char c = 'H';
+       	int Number3110 = 3110;   
+		int num0 = 0;
+		int num1 = 1;
+		
+		char w = 'w';
+		char r = 'r';
+		char d = 'd';
+		boolean b = true;
+		float f = 2.0f; 
+
+		String s8 = ""+c + Number3110+" "+w+num0+r+num1+d+" "+f+" "+b;
+        System.out.println(s8);
+
+    }
+}
+   
+
+   
